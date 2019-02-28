c53e0c1e8687aa7849c41a135e38d51fb30872b9
IV-I <aifai_5@yahoo.com>
2019-02-28 17:36:40 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+
+
+<html>
+<head>
+    <title>Search for a user:</title>
+</head>
+
+<body>
+<h2> Search for a user below:</h2><br/><br/>
+<form action="profile.php" method="GET">
+    <table>
+        <tr><td>Username:</td><td><input type="text" id="username" name="username"></td></tr>
+        <tr><td>
+                <input type="submit" id="submit" name="submit" value="View Profile!"></td></tr>
+    </table>
+</form>
+</body>
+</html>
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 26/02/2019
+ * Time: 12:07
+ */
